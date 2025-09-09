bd81d5d2ef37aa5db10628dd3a38501f2349d7da
talles <talles-guerra@hotmail.com>
2025-09-09 16:37:39 +0100

    Estrutrura de controle » Laços

--- Lógica com Python/estruturasDeControle.py
+++ Lógica com Python/estruturasDeControle.py
@@ -1,7 +1,47 @@
-""" 
+""" LAÇOS 
 Loop » repete a conduição ate q ela seja FALSA
     for » quando sabemos o numero de repetiçoes 
 
-    while » quando NAO sabemos o numero de repstiçoes    
+    
+Enquanto o contador(i) estiver entre 1 e 6
+    for i in range(1,6)
+    print (i)
+
+    i = 0
+for i in range(1, 10):
+    if i ==4 : break
+    print(i)
+                
+
+    while » quando NAO sabemos o numero de repetiçoes 
+        contador = 1 
+        while contador <= 5:
+            print(contador)
+            contador += 1
+    
+    
+
+
+senha = ""
+while senha != "1234":
+    senha = input("Senha incorreta, digite a senha: ")
+
+print("Acesso liberado.")     
+contador = 1
+
+while contador <= 10:
+    print(contador)
+    contador += 1 
+
+soma = 0
+for i in range(1, 101):
+    soma += i
+print("A soma de todos os numeros de 1 a 100 é : ", soma) 
 
 """
+
+
+""" ARRAYS E COLEÇÕES
+
+
+ """
